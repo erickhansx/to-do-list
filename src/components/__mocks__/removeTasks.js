0a002237de26fa421f32736f7aa46ed871b581b5
erickhansx <erickhansx@gmail.com>
2022-10-06 16:12:12 -0500

    Fix linter errors

--- src/components/__mocks__/removeTasks.js
+++ src/components/__mocks__/removeTasks.js
@@ -1,14 +1,14 @@
-import { storeGet } from "../__mocks__/localStorage";
+import { storeGet } from './localStorage.js';
 
 export default function removeTask(e, ei) {
-  let newArray = storeGet();
+  const newArray = storeGet();
   newArray.splice(ei.index, 1);
   // e.parentElement.remove();
-  let tasks = JSON.parse(localStorage.getItem("tasks"));
+  let tasks = JSON.parse(localStorage.getItem('tasks'));
   const newTasks = tasks.filter((task) => task.index != e.target.id); // eslint-disable-line
   tasks = newTasks;
   tasks.forEach((task, index) => {
     task.index = index;
   });
-  localStorage.setItem("tasks", JSON.stringify(newArray));
+  localStorage.setItem('tasks', JSON.stringify(newArray));
 }
