162a0249bb2d80e9ec55cc414ae9f1b3ad12dc91
erickhansx <erickhansx@gmail.com>
2022-10-09 17:41:20 -0500

    Add colors to list, button hover effect

---
+++ dist/main.61e9244d70d958393aaa.hot-update.js
@@ -0,0 +1,29 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+self["webpackHotUpdatewebpack2_0"]("main",{
+
+/***/ "./node_modules/css-loader/dist/cjs.js!./node_modules/sass-loader/dist/cjs.js!./src/components/addremove/style.scss":
+/*!**************************************************************************************************************************!*\
+  !*** ./node_modules/css-loader/dist/cjs.js!./node_modules/sass-loader/dist/cjs.js!./src/components/addremove/style.scss ***!
+  \**************************************************************************************************************************/
+/***/ (() => {
+
+eval("throw new Error(\"Module build failed (from ./node_modules/sass-loader/dist/cjs.js):\\nSassError: expected \\\"{\\\".\\n   ╷\\n22 │   font-weight: 600;\\r\\n   │                   ^\\n   ╵\\n  src\\\\components\\\\addremove\\\\style.scss 22:19  root stylesheet\");\n\n//# sourceURL=webpack://webpack2.0/./src/components/addremove/style.scss?./node_modules/css-loader/dist/cjs.js!./node_modules/sass-loader/dist/cjs.js");
+
+/***/ })
+
+},
+/******/ function(__webpack_require__) { // webpackRuntimeModules
+/******/ /* webpack/runtime/getFullHash */
+/******/ (() => {
+/******/ 	__webpack_require__.h = () => ("bdcecdc5ade2ae473b2e")
+/******/ })();
+/******/ 
+/******/ }
+);
